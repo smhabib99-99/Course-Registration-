4a6b0652064979b5555f81b9b855f1fdba01c301
smhabib99-99 <habibaust34@gmail.com>
2023-09-15 16:37:31 +0600

    private empty repository creation with heading named course registration and setup of react library done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-semibold text-center mt-8'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
