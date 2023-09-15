c953732dd27d9a1e21e43088104851b4a1e0b121
smhabib99-99 <habibaust34@gmail.com>
2023-09-15 18:58:18 +0600

    courses component file data loading from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Courses from './components/Courses/Courses'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl font-semibold text-center mt-8'>Course Registration</h1>
+
+      <Header></Header>
+      <Courses></Courses>
+
     </>
   )
 }
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,25 @@
+
+import { useState } from "react";
+import { useEffect } from "react";
+
+const Courses = () => {
+
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(()=>{
+
+        fetch('courses.json')
+        .then(res=>res.json())
+        .then(data=>setCourses(data))
+
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Courses;
